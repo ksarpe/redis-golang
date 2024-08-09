44d24499b77dd7a548ac8ec2dad1bd9949f6fd86
Kasper Janowski <2299kasper@gmail.com>
2024-08-09 22:18:47 +0200

    first commit

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/ksarpe/redis-golang
+
+go 1.22.5
---
+++ api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import(
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App){
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main(){
+	err := godotenv.Load()
+	app := fiber.New()
+}
